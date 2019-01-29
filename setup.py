22740571c20fe475806c5632e616485578bb87fe
Jan Gieseler <Jan.Gieseler@hotmail.com>
2019-01-29 14:00:28 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pynvcenter',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:JanGieseler/pynvcenter.git',
+    author='Jna Gieseler',
+    author_email='jangie@pm.me',
+    license='unlicense',
+    packages=['pynvcenter'],
+    zip_safe=False
+)
